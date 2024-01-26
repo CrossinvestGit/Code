09d900b17f1e71a056ac294f8fdb6883893cd3be
Adhurim Ramadani <adhurim_ramadani@hotmail.com>
2024-01-26 17:46:38 +0100

    Add signup functionality

--- 01_2024_Quantamental/quantamental/scores/urls.py
+++ 01_2024_Quantamental/quantamental/scores/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "scores"
 
 urlpatterns = [
+    path(route="signup/", view=views.SignUpView.as_view(), name="signup_view"),
     path(route="scores/", view=views.scores_view, name="scores_view"),
     path(route="test/", view=views.test_view, name="test_view"),
 ]
--- 01_2024_Quantamental/quantamental/scores/views.py
+++ 01_2024_Quantamental/quantamental/scores/views.py
@@ -1,7 +1,20 @@
+from django import forms
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
-from . import models
+# from . import models
+from .forms import UserRegisterForm
 
 
 # Create your views here.
@@ -13,3 +26,13 @@ def scores_view(request):
 @login_required
 def test_view(request):
     return render(request, "scores/test_view.html")
+
+
+class SignUpView(CreateView):
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy("login")
+    form_class = UserRegisterForm
+
+
+#  def  signup_view(request):
+#    return render(request, 'registration/signup.html', {'form': form})
