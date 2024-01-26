8fd7d35d8259c967b76a5d68d871454d78f3ffac
Adhurim Ramadani <adhurim_ramadani@hotmail.com>
2024-01-26 15:29:52 +0100

    Add login_required decorator to views.py and update login.html

--- 01_2024_Quantamental/quantamental/scores/views.py
+++ 01_2024_Quantamental/quantamental/scores/views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from . import models
 
 
 # Create your views here.
+@login_required
 def scores_view(request):
     return render(request, "scores/scores_view.html")
 
 
+@login_required
 def test_view(request):
     return render(request, "scores/test_view.html")
