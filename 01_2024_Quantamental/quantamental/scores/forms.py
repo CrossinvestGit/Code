6ffed280bd8e14be4005b00b24a2b07291326145
Adhurim Ramadani <adhurim_ramadani@hotmail.com>
2024-01-29 10:34:34 +0100

    Update CSS styles and form fields

--- 01_2024_Quantamental/quantamental/scores/forms.py
+++ 01_2024_Quantamental/quantamental/scores/forms.py
@@ -4,8 +4,16 @@ from django.contrib.auth.models import User
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ("username", "password1", "password2", "email")
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+
+        self.fields["username"].widget.attrs["class"] = "form-control"
+        self.fields["password1"].widget.attrs["class"] = "form-control"
+        self.fields["password2"].widget.attrs["class"] = "form-control"
+        self.fields["email"].widget.attrs["class"] = "form-control"
