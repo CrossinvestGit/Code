3dbfa9aeca43906e36289fc92ecb114b9cf6e30e
Adhurim Ramadani <adhurim_ramadani@hotmail.com>
2024-02-15 15:28:47 +0100

    Refactor table js - wrapping html table population and DataTable initialization in separate functions

--- 01_2024_Quantamental/quantamental/static/js/test2.js
+++ 01_2024_Quantamental/quantamental/static/js/test2.js
@@ -1,86 +1,92 @@
-// Get all table elements with class .table-general
 const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 const extractFieldName = (str) => str.split('__')[str.split('__').length - 1];
 
-const tables = document.querySelectorAll('.table-general');
-let tableIdCounter = 1;
-
-// TODO: Wrap this in function
-for (const table of tables) {
+const populateHtmlTable = async (table, tableIdCounter) => {
     const url = table.dataset.url;
-    axios.get(url)
-        .then(response => {
-            table.id += tableIdCounter;
-            tableIdCounter++;  // Increment counter
+    try {
+        const response = await axios.get(url);
+        table.id += tableIdCounter;
 
-            let data = response.data;
-            let tableHeaders = Object.keys(data[0]);
-            let tableHeadersHTML = '';
-            let tableBodyHTML = '';
+        const data = response.data;
+        const tableHeaders = Object.keys(data[0]);
+        const tableHeadersHTML = tableHeaders.map(header => `<th>${capitalize(extractFieldName(header))}</th>`).join('');
+        const tableBodyHTML = data.map(item => {
+            const rowHTML = tableHeaders.map(header => `<td>${item[header]}</td>`).join('');
+            return `<tr>${rowHTML}</tr>`;
+        }).join('');
 
-            for (const header of tableHeaders) {
-                const extractHeader = extractFieldName(header);
-                const capHeader = capitalize(extractHeader);
-                tableHeadersHTML += `<th>${capHeader}</th>`;
-            }
+        table.querySelector('.table-headers').innerHTML = tableHeadersHTML;
+        table.querySelector('.table-footer').innerHTML = tableHeadersHTML;
+        table.querySelector('.table-body').innerHTML = tableBodyHTML;
 
-            table.querySelector('.table-headers').innerHTML = tableHeadersHTML;
-            table.querySelector('.table-footer').innerHTML = tableHeadersHTML;
-
-            for (const item of data) {
-                let rowHTML = '';
-                for (const header of tableHeaders) {
-                    rowHTML += `<td>${item[header]}</td>`;
-                }
-                tableBodyHTML += `<tr>${rowHTML}</tr>`;
-            }
-            table.querySelector('.table-body').innerHTML = tableBodyHTML;
-
-
-            $(table).DataTable({
-                stateSave: true,
-                select: true,
-                pagingType: 'first_last_numbers',
-                dom: 'PBfrtip',
-                responsive: true,
-                colReorder: true,
-                keys: false,
-                searchPanes: {
-                    cascadePanes: true,
-                    initCollapsed: true,
-                },
-                buttons: [
-                    { extend: 'createState' },
-                    { extend: 'savedStates' },
-                    { extend: 'colvis', text: 'Column Visibility' },
-                    { extend: 'copy' },
-                    { extend: 'csv' },
-                    { extend: 'excel' },
-                    { extend: 'pdf', orientation: 'landscape' },
-                    { extend: 'print' },
-                ],
-                initComplete: function () {
-                    this.api()
-                        .columns()
-                        .every(function () {
-                            let column = this;
-                            let title = column.footer().textContent;
-                            let input = document.createElement('input');
-                            input.placeholder = title;
-                            column.footer().replaceChildren(input);
-                            input.addEventListener('input', () => {
-                                if (column.search() !== input.value) {
-                                    column.search(input.value).draw();
-                                }
-                            });
-                        });
-                }
-            });
-        })
-        .catch(error => {
-            console.error(error);
-        });
+        return table;
+    } catch (error) {
+        console.error(`Error populating table: ${error}`);
+    }
 };
 
+const initializeDataTable = (table) => {
+    $(table).DataTable({
+        stateSave: true,
+        select: true,
+        pagingType: 'first_last_numbers',
+        dom: 'PBfrtip',
+        responsive: true,
+        colReorder: true,
+        keys: false,
+        searchPanes: {
+            cascadePanes: true,
+            initCollapsed: true,
+        },
+        buttons: [
+            { extend: 'createState' },
+            { extend: 'savedStates' },
+            { extend: 'colvis', text: 'Column Visibility' },
+            { extend: 'copy' },
+            { extend: 'csv' },
+            { extend: 'excel' },
+            { extend: 'pdf', orientation: 'landscape' },
+            { extend: 'print' },
+        ],
+        initComplete: function () {
+            this.api()
+                .columns()
+                .every(function () {
+                    let column = this;
+                    let title = column.footer().textContent;
+                    let input = document.createElement('input');
+                    input.placeholder = title;
+                    column.footer().replaceChildren(input);
+                    input.addEventListener('input', () => {
+                        if (column.search() !== input.value) {
+                            column.search(input.value).draw();
+                        }
+                    });
+                });
+        }
+    });
+};
 
+// Does tables one after the other
 
+// const initializeTables = async () => {
+//     const tables = document.querySelectorAll('.table-general');
+//     let tableIdCounter = 1;
+//     for (const table of tables) {
+//         const htmltable = await populateHtmlTable(table, tableIdCounter);
+//         initializeDataTable(htmltable);
+//         tableIdCounter++;
+//     }
+// };
+
+// Does tables in parallel
+const initializeTables = async () => {
+    const tables = Array.from(document.querySelectorAll('.table-general'));
+    const promises = tables.map(async (table, index) => {
+        const htmltable = await populateHtmlTable(table, index + 1);
+        initializeDataTable(htmltable);
+    });
+    await Promise.all(promises);
+};
+
+initializeTables();
